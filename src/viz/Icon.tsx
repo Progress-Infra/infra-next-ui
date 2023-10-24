0e00481a222020ba6cfea12f9dd5aa0f0ff4f657
Alan Hedgeman <alan.hedgeman@progress.com>
2023-10-24 12:15:25 -0400

    icons

--- src/viz/Icon.tsx
+++ src/viz/Icon.tsx
@@ -1,16 +1,40 @@
 import * as React from "react";
-import { Icon as MIcon, IconProps as MIconProps } from "@mui/material";
+import { SvgIconProps } from "@mui/material";
+import { Menu, KeyboardArrowDown, KeyboardArrowUp } from "@mui/icons-material";
 
-export type IconProps = MIconProps;
+export type IconKeys = "hamburgerMenu" | "menuDown" | "menuUp";
 
-function Icon({ children, ...others }: IconProps): JSX.Element {
-    return (
-        <MIcon
-            {...others}
-        >
-            {children}
-        </MIcon>
-    )
+export type IconProps = SvgIconProps & {
+    iconKey: IconKeys
+};
+
+function getIcon({ iconKey, children, ...others }: IconProps): JSX.Element {
+    switch (iconKey) {
+        case "hamburgerMenu":
+            return <Menu
+                {...others}
+            >
+                {children}
+            </Menu>
+        case "menuDown":
+            return <KeyboardArrowDown
+                {...others}
+            >
+                {children}
+            </KeyboardArrowDown>
+        case "menuUp":
+            return <KeyboardArrowUp
+                {...others}
+            >
+                {children}
+            </KeyboardArrowUp>
+        default:
+            return <></>
+    }
+}
+
+function Icon(props: IconProps): JSX.Element {
+    return getIcon({ ...props });
 }
 
 export default Icon;
