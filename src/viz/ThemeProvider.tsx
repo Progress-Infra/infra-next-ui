cc799367f7fdc66d89892c56166c8473850edf0e
Alan Hedgeman <alan.hedgeman@progress.com>
2023-11-16 14:34:31 -0500

    some default styling

--- src/viz/ThemeProvider.tsx
+++ src/viz/ThemeProvider.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { ThemeProvider as MThemeProvider, createTheme } from "@mui/material/styles";
 import { CssBaseline, useMediaQuery } from "@mui/material";
+import { brandColor } from "./ThemeConstants";
 
 export interface ThemeProviderProps {
     children: React.ReactNode
@@ -8,12 +9,34 @@ export interface ThemeProviderProps {
 
 function ThemeProvider({ children }: ThemeProviderProps): JSX.Element {
     const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)'),
-        defaultTheme = localStorage.getItem("theme") === "dark" || prefersDarkMode ? "dark" : "light",
+        defaultTheme = localStorage.getItem("nui.theme") === "dark" || prefersDarkMode ? "dark" : "light",
         theme = React.useMemo(
             () =>
                 createTheme({
+                    components: {
+                        MuiButton: {
+                            styleOverrides: {
+                                root: {
+                                    textTransform: "none"
+                                }
+                            }
+                        }
+                    },
                     palette: {
-                        mode: defaultTheme
+                        mode: defaultTheme,
+                        ...(
+                            defaultTheme === "dark" ?
+                                {
+                                    primary: {
+                                        main: brandColor[50]
+                                    }
+                                } :
+                                {
+                                    primary: {
+                                        main: brandColor[100]
+                                    }
+                                }
+                        )
                     }
                 }),
             [defaultTheme]
